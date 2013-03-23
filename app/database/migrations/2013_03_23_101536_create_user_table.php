3c2a06e10cee8122017be298a6ac01ab3aca7c08
maticv <matic.volk@gmail.com>
2013-03-23 15:48:45 +0100

    user and macroservice table

---
+++ app/database/migrations/2013_03_23_101536_create_user_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateUserTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('user', function($table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('surname');
+			$table->string('email');
+			$table->string('time_zone');
+			$table->string('language');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('user');
+	}
+
+}
---
+++ app/database/migrations/2013_03_23_102329_create_macro_service_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMacroServiceTable extends Migration {
+
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('service', function($table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('location');
+			$table->integer('telephone_number');
+			$table->string('address');
+			$table->integer('ZIP_code');
+			$table->string('description');
+			$table->string('working_hours');
+			$table->timestamps();
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('service');
+	}
+
+}
