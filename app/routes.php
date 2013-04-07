93bb9cf9995a29aef31a610c6ec186e9dbb2c13f
riba1122 <riba1122@gmail.com>
2013-04-07 17:46:50 +0200

    cleared routes of balast

--- app/routes.php
+++ app/routes.php
@@ -15,7 +15,7 @@ Route::get('find', function()
 	return View::make('find');
 });
 
-<<<<<<< HEAD
+
 Route::get('registerUserError', function()
 {
 	return View::make('registerUserError');
@@ -23,9 +23,8 @@ Route::get('registerUserError', function()
 
 Route::resource('ureservation' , 'CustomerReservation');
 
-=======
+
 //TODO to user controller
->>>>>>> 03bc326e3711e3f4d278a05d271d5f79c390d7af
 Route::get('registerUser',function()
 {
 	return View::make('registerUser');
@@ -38,13 +37,10 @@ Route::resource('ManageServices','ManageServices');
 Route::get('/service/{id}/time','ManageServices@timetable');
 Route::get('/service/{id}/breaks','ManageServices@breaks');
 
-<<<<<<< HEAD
 Route::resource('riba','Riba');
 Route::resource('user','UserController');
 
-=======
 //provider controller
 Route::resource('provider','Provider');
 Route::get('provider/confirm/{token}','Provider@getConfirm');
 Route::post('provider/confirm/{token}','Provider@postConfirm');
->>>>>>> 03bc326e3711e3f4d278a05d271d5f79c390d7af
