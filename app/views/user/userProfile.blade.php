1ea8c058fed818a670381eb732032bd6ec3438b9
riba1122 <riba1122@gmail.com>
2013-04-17 00:19:32 +0200

    added userLibrary.php library

--- app/views/user/userProfile.blade.php
+++ app/views/user/userProfile.blade.php
@@ -7,16 +7,13 @@
 @section('content')
 
 <?php
+/*
 $id = Auth::user()->id;
 $name = Auth::user()->name;
 $surname = Auth::user()->surname;
 $email = Auth::user()->mail;
 $timezone = Auth::user()->time_zone;
 $language = Auth::user()->language;
-
-
-?>
-
 
 {{Former::label('Name')}}
 {{Former::label($name)}}
@@ -28,5 +25,21 @@ $language = Auth::user()->language;
 {{Former::label($timezone)}}
 {{Former::label('Language')}}
 {{Former::label($language)}}
+
+*/
+
+
+?>
+
+{{Former::label('Name')}}
+{{Former::label($user->name)}}
+{{Former::label('Surname')}}
+{{Former::label($user->surname)}}
+{{Former::label('Email')}}
+{{Former::label($user->email)}}
+{{Former::label('Timezone')}}
+{{Former::label($user->timezone)}}
+{{Former::label('Language')}}
+{{Former::label(UserLibrary::lang($user->language))}}
 
 @stop
