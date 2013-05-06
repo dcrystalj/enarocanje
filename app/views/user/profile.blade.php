07640a8600d40a2ae77588e408aa88aad6cc1650
dcrystalj <dcrystalj@gmail.com>
2013-05-06 20:01:09 +0200

    fix to blade syntax profile

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -10,8 +10,15 @@
 {{Former::label(Lang::get('user.surname').': '.$user->surname)}}
 {{Former::label(Lang::get('user.email').': '.$user->email)}}
 {{Former::label(Lang::get('user.timezone').': '.$user->time_zone)}}
-<?php if($user->language != null) Former::label(Lang::get('user.language').': '.UserLibrary::language($user->language))
-?>
-{{Button::link(URL::route('user.edit', Auth::user()->id), Lang::get('user.editSettings')) }}
-{{Button::link(URL::route('referral.create'), Lang::get('user.refer'))}}
+
+@if(is_int($user->language)) 
+{{ Former::label(Lang::get('user.language').': '.
+	UserLibrary::language($user->language)) }}
+@endif
+
+{{Button::link(URL::route('user.edit', Auth::user()->id), 
+		Lang::get('user.editSettings')) }}
+
+{{Button::link(URL::route('referral.create'), 
+		Lang::get('user.refer'))}}
 @stop
