2644898b46a2705831059ba3759fdb1c4f797881
maticv <matic.volk@gmail.com>
2013-05-06 21:28:22 +0200

    admin referral view

---
+++ app/views/referral/index.blade.php
@@ -0,0 +1,33 @@
+@extends('layouts.default')
+
+@section('title')
+    Refer a friend
+@stop
+
+@section('content')
+
+    
+	<?php 
+        $referrals = Referrals::all();
+        $tbody = []; 
+        $i = 1; 
+        foreach ($referrals as $ref){
+        	$user1 = User::find($ref->referral_id);
+        	$user2 = User::find($ref->new_user_id);
+
+            $tbody[]= [
+                'id'     => $i, 
+                'emailR'   => $user1->email,
+                'emailN'   => $user2->email,
+             ];
+             $i++;
+        }
+    ?>
+
+    {{ Table::hover_open(["class"=>'sortable']) }}
+    {{ Table::headers('#', 'Provider','Invited provider', '') }}
+    {{ Table::body($tbody) }}
+    {{ Table::close() }}
+
+
+@stop
