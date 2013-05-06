6488c452c0e68e356ebc41a5cc25ea2f660e869b
dcrystalj <dcrystalj@gmail.com>
2013-05-06 19:02:48 +0200

    fix ribas translation page broken

--- app/views/macro/create.blade.php
+++ app/views/macro/create.blade.php
@@ -43,26 +43,6 @@
         @endif
         </div>  
     </div>
-    
-
-
-    
-     @if(isset($mac))
-        {{ Former::open(URL::route('macro.update', $mac->id ))->method('PUT')->rules($rules) }}
-        {{ Former::populate($mac) }}
-    @else
-        {{ Former::open(URL::route('macro.store'))->rules($rules) }}
-    @endif
-    {{ Former::select('name',Lang::get('services.name').': ')->options(Service::categories())->autofocus() }}
-    {{ Former::select('ZIP_code',Lang::get('services.zipCode').':')->options($codes)}}
-    {{ Former::text('street',Lang::get('services.service').': ')}}
-    {{ Former::text('email',Lang::get('services.email').': ')->value(Auth::user()->email)}}
-    {{ Former::text('telephone_number',Lang::get('services.telephoneNumber').': ')}}
-    {{ Former::text('site_url',Lang::get('services.urlToYourSite').': ')}}
-    {{ Former::textarea('description',Lang::get('services.description').': ')->rows(10)->columns(20) }}
-    {{ Former::actions()->submit( isset($mac) ? Lang::get('services.saveChanges') : Lang::get('services.addService')) }}
-    {{ Former::close() }}
-
-    
+   
 
 @stop
