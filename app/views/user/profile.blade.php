d1622aa4e5e11b023b1bfee5661214b97bb4e881
dcrystalj <dcrystalj@gmail.com>
2013-05-09 13:19:13 +0200

    refactoring user

--- app/views/user/profile.blade.php
+++ app/views/user/profile.blade.php
@@ -5,18 +5,23 @@
 @stop
 
 @section('content')
+
 @if(!empty($user->name)) 
-{{Former::label(Lang::get('general.name').': '.$user->name)}}
+	{{Former::label(Lang::get('general.name').': '.$user->name)}}
 @endif
+
 @if(!empty($user->surname)) 
-{{Former::label(Lang::get('general.surname').': '.$user->surname)}}
+	{{Former::label(Lang::get('general.surname').': '.$user->surname)}}
 @endif
+
 {{Former::label(Lang::get('general.email').': '.$user->email)}}
+
 @if(is_int($user->timezone)) 
-{{Former::label(Lang::get('general.timezone').': '.UserLibrary::timezone($user->time_zone))}}
+	{{Former::label(Lang::get('general.timezone').': '.UserLibrary::timezone($user->time_zone))}}
 @endif
+
 @if(is_int($user->language)) 
-{{ Former::label(Lang::get('general.language').': '.
+	{{ Former::label(Lang::get('general.language').': '.
 	UserLibrary::language($user->language)) }}
 @endif
 
@@ -27,4 +32,5 @@
 		Lang::get('general.refer'))}}
 
 {{Button::link(URL::to('/google/export/reservations'), Lang::get('general.exportReservations')) }}
+
 @stop
