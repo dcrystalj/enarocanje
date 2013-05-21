66c40ebf8d0cac09999d2ecc1eb0dff522c26a51
riba1122 <riba1122@gmail.com>
2013-05-21 21:59:31 +0200

    fixed fc something

--- public/js/fc/fullcalendar.js
+++ public/js/fc/fullcalendar.js
@@ -64,7 +64,7 @@ var defaults = {
 	monthNames: ['January','February','March','April','May','June','July','August','September','October','November','December'],
 	monthNamesShort: ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'],
 	dayNames: ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday'],
-	dayNamesShort: ['Sun',trans('general.mon'),'Tue','Wed','Thu','Fri','Sat'],
+	dayNamesShort: ['Sun','Mon','Tue','Wed','Thu','Fri','Sat'],
 	buttonText: {
 		prev: "<span class='fc-text-arrow'>&lsaquo;</span>",
 		next: "<span class='fc-text-arrow'>&rsaquo;</span>",
