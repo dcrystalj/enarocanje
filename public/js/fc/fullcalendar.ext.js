b5dcfbcff4b09f9fa14d214a2d64a09c5175d59a
dcrystalj <dcrystalj@gmail.com>
2013-05-26 23:37:43 +0200

    to Friday

--- public/js/fc/fullcalendar.ext.js
+++ public/js/fc/fullcalendar.ext.js
@@ -454,6 +454,20 @@ $(function(){
 			
 		});
 	}
+	$('#tofriday').click(function(e){
+		e.preventDefault();
+		var start 	= $('#datetimepick'+1+''+1+' input').val();
+		var end 	= $('#datetimepick'+1+''+2+' input').val();
+
+		for(var j=2; j<=5; j++) {
+						
+			$('#datetimepick'+j+''+1+' input').val(start);
+			$('#datetimepick'+j+''+2+' input').val(end);
+
+
+		}				
+			
+	});
 });
 
 ///////
